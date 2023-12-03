979cdbb339daee17b110ba53a4f65fbfdf0770ab
company in phone <122741180+defbothub@users.noreply.github.com>
2023-12-03 21:16:59 +0200

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,6 +1,6 @@
 BOT_TOKEN = '6266536188:AAF2JPFNBI7yHAdAqzYHXVZdSmHebhKkU6Q'
 
-ADMINS = [6128561399]
+ADMINS = [5791823682]
 #Адмін 6128561399, крмпанія в смартфоні 5791823682
 
 #DATABASE_URL = 'postgres://uhiiehvmiqvcav:0bb9bd695847d63b4e04dc722a4d103dccc1ed46fb9782e730548bda02b3218a@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/d9u2koal6bthk5'
