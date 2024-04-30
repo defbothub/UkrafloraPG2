00bfc722dbadf699d5205aa75672237e7dd60988
company in phone <122741180+defbothub@users.noreply.github.com>
2024-04-30 12:09:46 +0300

    Update db_loader.py

--- utils/db/db_loader.py
+++ utils/db/db_loader.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, Session
 
-DATABASE_URL = 'postgresql://tlsonjxxxrjutf:fa7a42d08c48a7c979c96535a3277244dde9acf0039da9ac143cc098901ed3cd@ec2-176-34-212-157.eu-west-1.compute.amazonaws.com:5432/d5dp934vr7psm7'
+DATABASE_URL = 'postgresql://rxnotqepljlmpp:e8205400e6f8d44b9b5799c1850f3aa996feb260bd16120a0088e3e2ea969772@ec2-63-35-80-199.eu-west-1.compute.amazonaws.com:5432/d5jh23sj9fkhpv'
 
 engine = create_engine(DATABASE_URL)
 engine.connect()
